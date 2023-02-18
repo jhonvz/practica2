9c6a9f0a5d1f779c0e288e2addd85048ccd18317
jhonvz <velasquezjhonatan7@gmail.com>
2023-02-17 19:22:15 -0600

    first commit

---
+++ practica2/practica2/Models/Equipo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace practica2.Models
+{
+    public class Equipo
+    {
+        [Name("Nombre")]
+        public String nombre { get; set; }
+        [Name("Coach")]
+        public String coach { get; set; }
+        [Name("Liga")]
+        public String liga { get; set; }
+        [Name("Fecha_creacion")]
+        public String creacion { get; set; }
+
+        public Equipo()
+        { }
+        public Equipo(String nom, String Coach, String Liga, String Creacion)
+        {
+            this.nombre = nom;
+            this.coach = Coach;
+            this.liga = Liga;
+            this.creacion = Creacion;
+        }
+    }
+}
---
+++ practica2/practica2/Models/Jugador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace practica2.Models
+{
+    public class Jugador
+    {
+        [Name("Nombre")]
+        public String nombre { get; set; }
+        [Name("Apellido")]
+        public String apellido { get; set; }
+        [Name("Rol")]
+        public String rol { get; set; }
+        [Name("KDA")]
+        public double kda { get; set; }
+        [Name("CS")]
+        public float cs { get; set; }
+        [Name("Equipo")]
+        public String equipo { get; set; }
+        public Jugador()
+        { }
+        public Jugador(String nom, String ap, String Rol, double KDA, float CS, String Equipo)
+        {
+            this.nombre = nom;
+            this.apellido = ap;
+            this.rol = Rol;
+            this.kda = KDA;
+            this.cs = CS;
+            this.equipo = Equipo;
+        }
+    }
+}
